ff8cddca3986b1440a357ec0c094080a3f2e4ce2
Joe Calandra <jjcx82@gmail.com>
2023-04-14 16:17:11 -0400

    Added Death/Damage Animations, new version of healthbar

--- Assets/ProjectV/Scripts/Player/Health.cs
+++ Assets/ProjectV/Scripts/Player/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 public class Health : NetworkBehaviour
 {
@@ -10,25 +11,56 @@ public class Health : NetworkBehaviour
     [SyncVar(hook = nameof(OnHealthChange))]
     public int maxHealth;
     
+    
     [SerializeField]
-    private GameObject healthBarPrefab;
-    private GameObject healthBar;
-    private SpriteRenderer barRender;
+    private SpriteRenderer callerRender;
 
-    private Vector2 tempScale;
+    private Color originalColor;
+    private GameObject healthbarUI;
+    private bool colorSaved;
+    private Slider slider;
+    private Image fill;
+
     void Start()
     {
         if (isServer) 
         {
             health = maxHealth;
         }
-
-        healthBar = Instantiate(healthBarPrefab, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + (gameObject.GetComponent<Renderer>().bounds.size.y), 0), Quaternion.identity);
-        healthBar.transform.SetParent(gameObject.transform);   
-        barRender = healthBar.transform.GetChild(0).GetComponent<SpriteRenderer>();
-        UpdatePlayerHealthBar();
+        if (isLocalPlayer)
+        {
+            healthbarUI = GameObject.Find("HealthBar 1");
+            slider = healthbarUI.GetComponent<Slider>();
+            fill = healthbarUI.transform.GetChild(0).GetComponent<Image>();
+            SetMaxHealth(maxHealth);
+            //healthbarUI = GameObject.Find("HealthBar 1");
+            //healthbarUI.GetComponent<UIHealthBar>().SetMaxHealth(maxHealth);
+        }
+        /*healthbarUI = GameObject.Find("HealthBar 1");
+        slider = healthbarUI.GetComponent<Slider>();
+        fill = healthbarUI.transform.GetChild(0).GetComponent<Image>();
+        callerRender = gameObject.transform.GetComponent<SpriteRenderer>();
+        SetMaxHealth(maxHealth);*/
+        callerRender = gameObject.transform.GetComponent<SpriteRenderer>();
+        colorSaved = false;
+    }
+    void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
     }
 
+    void SetHealth(int health)
+    {
+        slider.value = health;
+        if (slider.value <= (slider.maxValue * .25f)){
+            fill.color = Color.red;
+        }else if(slider.value <= (slider.maxValue * .5f)){
+            fill.color = Color.yellow;
+        }else{
+            fill.color = Color.green;
+        }
+    }
     void OnHealthChange(int oldHealth, int newHealth)
     {
         if (isClientOnly)
@@ -36,10 +68,16 @@ public class Health : NetworkBehaviour
 
         if (isServer)
         {
-            print("Server: Player object health changed from " + oldHealth + " to " + newHealth);
+            print("Server: Object health changed from " + oldHealth + " to " + newHealth);
         }
-
-        UpdatePlayerHealthBar();
+        if (colorSaved == false)
+        {
+            originalColor = gameObject.transform.GetComponent<SpriteRenderer>().color;
+            colorSaved = true;
+        }
+        healthChangeAnimations(oldHealth, newHealth);
+        SetHealth(health);
+        //healthbarUI.GetComponent<UIHealthBar>().SetHealth(newHealth);
     }
 
     void OnMaxHealthChange(int oldMaxHealth, int newMaxHealth) 
@@ -49,48 +87,96 @@ public class Health : NetworkBehaviour
 
         if (isServer)
         {
-            print("Server: Player object max health changed from " + oldMaxHealth + " to " + newMaxHealth);
+            print("Server: Object max health changed from " + oldMaxHealth + " to " + newMaxHealth);
         }
-
-        UpdatePlayerHealthBar();
+        SetMaxHealth(maxHealth);
+        //healthbarUI.GetComponent<UIHealthBar>().SetMaxHealth(newMaxHealth);
     }
 
-    void UpdatePlayerHealthBar() 
+    void healthChangeAnimations(int oldHealth, int newHealth) 
     {
-        tempScale = healthBar.transform.GetChild(0).localScale;
-
-        if (health <= 0)
+        Color tmp = originalColor;
+        callerRender.color = tmp;
+        if (newHealth <= 0)
         {
-            tempScale.x = 0;
+            DeathAnimation();
+        }
+        else if (newHealth < oldHealth)
+        {
+            tmp.r = tmp.r * 1.5f;
+            tmp.g = tmp.g / 1.5f;
+            tmp.b = tmp.b / 1.5f;
+            callerRender.color = tmp;
+            HealthDownAnimation();
+        }
+        else if (newHealth > oldHealth)
+        {
+            tmp.g = tmp.g * 1.5f;
+            tmp.r = tmp.r / 1.5f;
+            tmp.b = tmp.b / 1.5f;
+            callerRender.color = tmp;
+            HealthUpAnimation();
+        }
+    }
+    void HealthUpAnimation()
+    {
+        Color tmp = callerRender.color;
+        tmp.g = tmp.g * .90f;
+        tmp.r = tmp.r / .90f;
+        tmp.b = tmp.b / .90f;
+        callerRender.color = tmp;
+        if (tmp.g <= originalColor.g)
+        {
+            callerRender.color = originalColor;
         }
         else
         {
-            tempScale.x = (float)health / (float)maxHealth;
+            Invoke("HealthUpAnimation", .075f);
         }
-        if (tempScale.x > 0.66)
-        {
-            barRender.color = Color.green;
-        }
-        else if (tempScale.x > 0.33f)
-        {
-            barRender.color = Color.yellow;
-        }
-        else 
-        {
-            barRender.color = Color.red;
-        }
-        
-        healthBar.transform.GetChild(0).transform.localScale = tempScale;
-
     }
+    void HealthDownAnimation()
+    {
+        Color tmp = callerRender.color;
+        tmp.r = tmp.r * .90f;
+        tmp.g = tmp.g / .90f;
+        tmp.b = tmp.b / .90f;
+        callerRender.color = tmp;
+        if (tmp.r <= originalColor.r)
+        {
+            callerRender.color = originalColor;
+        }
+        else
+        {
+            Invoke("HealthDownAnimation", .075f);
+        }
+    }
+    void DeathAnimation()
+    {
+        Color tmp = callerRender.color;
+        tmp.a = tmp.a * .9f;
+        tmp.r = tmp.r * 1.5f;
+        tmp.g = tmp.g * .5f;
+        tmp.b = tmp.b * .5f;
+        callerRender.color = tmp;
+        if (tmp.a < .1)
+        {
+            //Destroy(gameObject);
+            callerRender.color = originalColor;//For Testing
 
+        }
+        else
+        {
+            Invoke("DeathAnimation", .075f);
+        }
+    }
     [Server]
     public void ApplyDamage(int amount)
     {
         if ((health - amount) <= 0)
         {
             print("Player object has died. Setting health back to full");
-            health = maxHealth;
+            health -= amount;
+            //health = maxHealth;
         }
         else 
         {
